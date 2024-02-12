d9de576ea8ec4be08ec57a0c7200aef69641b636
eldossjogy <eldossjogy@gmail.com>
2024-02-11 19:28:51 -0500

    Initial commit

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import "./index.css";
+
+function App() {
+
+  return (
+    <div className="App">
+    <h1 className="text-5xl underline">Hello World</h1>
+    </div>
+  );
+}
+
+export default App;
