7cfc6048184f66de2b5dac02fd3ada204601e0b6
eldossjogy <eldossjogy@gmail.com>
2024-02-25 18:21:08 -0500

    updated files to jsx using rfc

--- client/src/authAndContext/contextApi.jsx
+++ client/src/authAndContext/contextApi.jsx
@@ -2,7 +2,6 @@ import React, { useState, useEffect, createContext } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { toast } from 'react-toastify';
 
-
 const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
