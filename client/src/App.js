9e02b4bb00ec1ebdf5b8c909e749a87a6e715677
aminnausin <aminnausin@outlook.com>
2024-02-25 18:49:05 -0500

    clean up merge

--- client/src/App.js
+++ client/src/App.js
@@ -4,23 +4,8 @@ import { Routes, Route } from 'react-router-dom';
 import HomePage from "./pages/HomePage";
 import MyProfile from "./pages/MyProfile";
 import AdminDashboard from "./pages/AdminDashboard";
-import { useEffect, useState } from "react";
-import { createClient } from "@supabase/supabase-js";
-
-const supabase = createClient("https://jjcsqjjvzatkopidmaha.supabase.co", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpqY3Nxamp2emF0a29waWRtYWhhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDg1NTk2OTcsImV4cCI6MjAyNDEzNTY5N30.9sby3xIuzizJGRTmBawKI6ra3_0CucTB8AvrU1TX0VA");
 
 function App() {
-
-  const [countries, setCountries] = useState([]);
-
-    useEffect(() => {
-      getCountries();
-    }, []);
-
-    async function getCountries() {
-      const { data } = await supabase.from("countries").select();
-      setCountries(data);
-    }
 
   // protected routes such as login required routes and admin dashboard route will be protected later. 
   // not a high priority right now
@@ -34,14 +19,6 @@ function App() {
         </Route>
     </Routes>
   );
-}
-
-function ToolbarButton(props) {
-  let colourStyle = props.primary ? 'ring-yellow-500 hover:bg-yellow-400 ' : 'ring-blue-500 hover:bg-blue-500 hover:text-white '
-  let style = colourStyle + 'ring-2 rounded-2xl px-4 py-1';
-  return (
-    <button className={style}>{props.value}</button>
-  )
 }
 
 export default App;
--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,7 +1,55 @@
+import "../index.css";
 import React from 'react'
+import Navbar from "../components/Navbar"
+import { useEffect, useState } from "react";
+import { createClient } from "@supabase/supabase-js";
+
 
 export default function HomePage() {
+  const [countries, setCountries] = useState([]);
+
+  useEffect(() => {
+    getCountries();
+  }, []);
+
+  async function getCountries() {
+    setCountries([{name:'can'},{name:'ban'},{name:'ind'}]);
+  }
+
   return (
-    <div>HomePage</div>
+    <div className="App">
+      <Navbar></Navbar>
+      <main className="container m-auto">
+        <section id="toolbar" className="flex shadow bg-neutral-100 rounded-xl w-full justify-between p-4 items-center my-4">
+          <div id="filters" className="flex space-x-4 text-xl justify-center items-center">
+            <ToolbarButton primary={true} value={"Automobiles"} />
+            <ToolbarButton primary={false} value={"Textbooks"} />
+            <ToolbarButton primary={false} value={"Recently Added"} />
+            <ToolbarButton primary={false} value={"Electronics"} />
+            <ToolbarButton primary={false} value={"Supplies"} />
+          </div>
+          <div id="controls">
+            <ToolbarButton primary={false} value={"X"} />
+          </div>
+        </section>
+        <section id="sidebar" className="w-[20%] h-[80vh] bg-slate-300 rounded-lg shadow-lg">
+          <ul>
+            {countries.map((country) => (
+              <li key={country.name}>{country.name}</li>
+            ))}
+          </ul>
+        </section>
+
+        <section id="content"></section>
+      </main>
+    </div>
+  )
+}
+
+function ToolbarButton(props) {
+  let colourStyle = props.primary ? 'ring-yellow-500 hover:bg-yellow-400 ' : 'ring-blue-500 hover:bg-blue-500 hover:text-white '
+  let style = colourStyle + 'ring-2 rounded-2xl px-4 py-1';
+  return (
+    <button className={style}>{props.value}</button>
   )
 }
