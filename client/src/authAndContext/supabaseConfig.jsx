d89e0a276115f6575b97e24dfaeae5f446f469a7
muyeed10 <muyeedh05@gmail.com>
2024-02-27 15:08:57 -0500

    added env variables in supabase config file

--- client/src/authAndContext/supabaseConfig.jsx
+++ client/src/authAndContext/supabaseConfig.jsx
@@ -3,8 +3,6 @@ import { createClient } from "@supabase/supabase-js";
 const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
 const supabaseKey = process.env.REACT_APP_SUPABASE_KEY;
 
-console.log(supabaseKey,supabaseUrl)
-
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase
