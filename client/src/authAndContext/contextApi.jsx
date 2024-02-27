e2960936103e84715194e71e486d05d5c6e343a6
muyeed10 <muyeedh05@gmail.com>
2024-02-27 15:08:09 -0500

    added env variables in supabase config file

--- client/src/authAndContext/contextApi.jsx
+++ client/src/authAndContext/contextApi.jsx
@@ -1,13 +1,32 @@
 import React, { useState, useEffect, createContext } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { toast } from 'react-toastify';
+import supabase from './supabaseConfig';
+
 
 const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
 
+    const [user, setUser] = useState({})
+
+    async function signUp(email, password, name) {
+        try {
+          let response = await supabase.auth.signUp({
+            email: email,
+            password: password,
+            name: name
+          });
+          console.log(response.data)
+          setUser(response.data)
+
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
     return (
-        <AuthContext.Provider value={{}} >
+        <AuthContext.Provider value={{signUp}} >
            {children}
         </AuthContext.Provider>
     )
