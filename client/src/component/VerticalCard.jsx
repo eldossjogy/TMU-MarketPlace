be314ec62bf02009229b2f103402e2c9c4040cce
Eldoss Jogy <eldossjogy@gmail.com>
2024-02-28 15:07:05 -0500

    Added image carousel for multiple images

--- client/src/component/VerticalCard.jsx
+++ client/src/component/VerticalCard.jsx
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import ImageCarousel from "./ImageCarousel";
 
 export default function VerticalCard({
   image,
@@ -9,17 +10,23 @@ export default function VerticalCard({
   location,
   postID
 }) {
+  const [hovered, setHovered] = useState(false);
   return (
-    <Link to={{pathname: `/${postID}`}}
+    <Link to={{ pathname: `/${postID}` }}
     >
-      <div id="card" className="hover:cursor-pointer">
+      <div id="card" className="hover:cursor-pointer" onMouseEnter={() => setHovered(true)} onMouseLeave={() => setHovered(false)}>
         <div className="bg-[#fafafb] m-3 rounded-lg border-2 border-gray shadow-md hover:shadow-lg overflow-hidden">
           <div className="mr-auto ml-auto rounded-md bg-[#fafafb] p-4">
-            <img
-              className="rounded-md w-full h-auto object-cover  aspect-square"
-              src={image}
-              alt="img"
-            ></img>
+            {Array.isArray(image) ?
+              <ImageCarousel images={image} hovered={hovered} setHovered={setHovered} />
+              :
+              <img
+                className="rounded-md w-full h-auto object-cover  aspect-square"
+                src={image}
+                alt="img"
+              ></img>
+
+            }
           </div>
           <div className="px-4 pb-4 bg-[#fafafb]">
             <div className="flex justify-between pb-2">
