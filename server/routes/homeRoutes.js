221aa56f487690f0657dd5273ee3e494961be1e3
muyeed10 <muyeedh05@gmail.com>
2024-02-29 15:58:13 -0500

    configured bakend baseline and folder structure

---
+++ server/routes/homeRoutes.js
@@ -0,0 +1,9 @@
+import express from "express";
+
+const router = express.Router();
+
+router.put('/get-all-ads', () => {console.log("came to home getAllAds route")})
+
+export default router;
+
+
---
+++ server/server.js
@@ -0,0 +1,23 @@
+import express from 'express';
+import cors from "cors";
+import dotenv from "dotenv";
+import userRoutes from "./routes/userRoutes.js";
+import adminRoutes from "./routes/adminRoutes.js";
+import homeRoutes from "./routes/homeRoutes.js";
+
+dotenv.config();
+
+const app = express();
+const port = process.env.port || 5000;
+
+//.cors() so that our front end will be able to talk to the backend
+app.use(cors());
+app.use(express.json());
+
+app.use('/user', userRoutes);
+app.use('/admin', adminRoutes);
+app.use('/home', homeRoutes);
+
+server.listen(port, () => {
+  console.log(`server is now running on port ${port}`);
+});
