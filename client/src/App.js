f9a94922213aa34b66a5ed85dd43da4ce510214e
aminnausin <aminnausin@outlook.com>
2024-02-29 14:12:26 -0500

    add example env and add nav example to routes

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import "./index.css";
 import React, {useContext} from 'react';
 import { Routes, Route } from 'react-router-dom';
 import HomePage from "./pages/HomePage";
+import HomePageNav from "./pages/HomePageNav";
 import MyProfile from "./pages/MyProfile";
 import AdminDashboard from "./pages/AdminDashboard";
 import { createClient } from "@supabase/supabase-js";
@@ -16,6 +17,7 @@ function App() {
     <Routes>
        <Route path = "/">
           <Route index element={<HomePage />}/>
+          <Route path = "nav" element={<HomePageNav />}/>
           <Route path = "my-market" element={<MyProfile />}/>
           <Route path = "admin-dashboard" element={<AdminDashboard />}/>
           <Route path = "/login" element={<LoginPage />}/>
