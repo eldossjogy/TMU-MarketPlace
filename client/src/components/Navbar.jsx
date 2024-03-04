06b23e6180c5f23b7d68676cddb07274987ca449
eldossjogy <eldossjogy@gmail.com>
2024-03-03 22:51:57 -0500

    added /setting

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -14,7 +14,7 @@ export default function Navbar() {
 
     const navigate = useNavigate();
 
-    const authOptions = [{name: 'Your Market', url: '/'},{name: 'Your Profile', url: '/'},{name: 'Your Inbox', url: '/'},{name: 'Saved Listings', url: '/'},{name: 'Log out', url: '/logout'}];
+    const authOptions = [{name: 'Your Market', url: '/'},{name: 'Your Profile', url: '/settings'},{name: 'Your Inbox', url: '/'},{name: 'Saved Listings', url: '/'},{name: 'Log out', url: '/logout'}];
     const unauthOptions = [{name: 'Log in', url: '/login'}, {name: 'Register', url: '/register'}];
 
     function success(pos) {
---
+++ client/src/pages/AccountSettings.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Navbar from "../components/Navbar";
+
+export default function AccountSettings() {
+  return (
+    <div>
+      <Navbar />
+      <div className="flex justify-center items-center my-3 mx-3">
+      <div className="bg-card p-3 rounded-lg w-full max-w-7xl shadow-md">
+    
+      </div>
+    </div>
+    </div>
+  );
+}
