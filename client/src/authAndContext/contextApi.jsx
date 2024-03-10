2c1cdc19c7f331b7095f7b89a0cf671975e71088
muyeed10 <muyeedh05@gmail.com>
2024-03-10 15:15:41 -0400

    cleaned up some code

--- client/src/authAndContext/contextApi.jsx
+++ client/src/authAndContext/contextApi.jsx
@@ -257,8 +257,7 @@ export const AuthProvider = ({ children }) => {
   async function createNewListing(listingInfo, imageList) {
     try {
 
-      const listOfImages = await uploadImageToBucket(imageList, "avatars")
-      console.log(listOfImages)
+      const listOfImages = await uploadImageToBucket(imageList, "ad-listings")
       const response = await axios.post(`${process.env.REACT_APP_BACKEND_API_URL}/my-market/create-new-listing`,
         {
           ...listingInfo,
