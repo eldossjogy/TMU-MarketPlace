fc879fa32e4f735451ed57b7d21e1044d2bfe434
muyeed10 <muyeedh05@gmail.com>
2024-03-10 20:03:18 -0400

    added links in /my-market page

--- client/src/App.js
+++ client/src/App.js
@@ -32,7 +32,7 @@ function App() {
             <Route path = "/my-market">
               <Route index element={<MyProfile />} />
               <Route path="sold-listings" element={<HomePage />} />
-              <Route path="create-listings" element={<CreateListings />} />
+              <Route path="create-listing" element={<CreateListings />} />
               <Route path="repost-listings" element={<HomePage />} />
             </Route>
           </Route>
