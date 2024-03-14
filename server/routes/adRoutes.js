117c13a5272891ebfd49711070aadf4bb9696bda
eldossjogy <eldossjogy@gmail.com>
2024-03-13 21:48:46 -0400

    moved to adAPI.js controller

--- server/routes/adRoutes.js
+++ server/routes/adRoutes.js
@@ -1,66 +1,15 @@
 import express from "express";
-import supabase from "../config/supabaseConfig.js";
+import { homepage, getUserAds, getByID } from "../controllers/adAPI.js";
 
 const router = express.Router();
 
-// get your ads
-router.get("/me", async (req, res) => {
-  let { user_id } = req.query;
-  try {
-    const myListings = await supabase
-      .from("ad")
-      .select("*")
-      .eq("user_id", user_id);
-    res.status(200).json(myListings);
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-});
-
 // get ads by id
-router.get("/", async (req, res) => {
-  let { id } = req.query;
-  try {
-    const postData = await supabase
-      .from("ad")
-      .select(
-        `
-        *,
-        image!inner(file_path),
-        category!inner(name),
-        status!inner(type),
-        profile!inner(id,name,avatar_url,postal_code)
-        `
-      )
-      .eq("id", id);
-    res.status(200).json(postData.data);
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-});
+router.get("/", getByID);
+
+// get user ads
+router.get("/me", getUserAds);
 
 // get homepage ads
-router.get("/homepage", async (req, res) => {
-  try {
-    const postData = await supabase.from("ad").select(
-      `
-        id,
-        title,
-        price,
-        description,
-        postal_code,
-        longitude,
-        latitude,
-        post_time,
-        image!inner(file_path),
-        category!inner(name),
-        status!inner(type)
-        `
-    );
-    res.status(200).json(postData.data);
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-});
+router.get("/homepage", homepage);
 
 export default router;
