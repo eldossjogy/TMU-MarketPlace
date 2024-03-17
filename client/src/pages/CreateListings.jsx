7c44b47f2cd4780b930c29dca18a96919cdab3df
muyeed10 <muyeedh05@gmail.com>
2024-03-17 12:25:25 -0400

    create-listing page front-end

--- client/src/pages/CreateListings.jsx
+++ client/src/pages/CreateListings.jsx
@@ -89,55 +89,61 @@ export default function CreateListings() {
         <div>
             <Navbar />
             <MyProfileContainer>
-            <form onSubmit={handleNewPost} className="createListingForm p-8 bg-white shadow-lg rounded-lg">
-                <div className='leftFormSection'>
-                    <label className="block mb-2">Title:</label>
-                    <input type="text" name="title" value={formData.title} onChange={handleChange} placeholder="Enter Title" className="block w-full border-gray-300 rounded-md focus:ring-indigo-500 focus:border-indigo-500 mb-4 p-2" />
+            <form onSubmit={handleNewPost} className="createListingForm p-8 bg-white shadow-md rounded-lg">
+                <h1 className='formTitle'>Create A New Listing</h1>
+                <div className='topFormSection'>
+                    <div className='leftFormSection'>
+                        <label className="block mb-2">Title:</label>
+                        <input autoComplete='off' type="text" name="title" value={formData.title} onChange={handleChange} placeholder="Enter Title" className="block w-full border-gray-300 rounded-md focus:ring-indigo-500 focus:border-indigo-500 mb-4 p-2" />
 
-                    <label className="block mb-2">Price:</label>
-                    <input type="text" name="price" value={formData.price} onChange={handleChange} placeholder="Enter Price" className="block w-full border-gray-300 rounded-md focus:ring-indigo-500 focus:border-indigo-500 mb-4 p-2" />
+                        <label className="block mb-2">Price:</label>
+                        <input autoComplete="off" type="text" name="price" value={formData.price} onChange={handleChange} placeholder="Enter Price" className="block w-full border-gray-300 rounded-md focus:ring-indigo-500 focus:border-indigo-500 mb-4 p-2" />
 
-                    <label className="block mb-2">Postal Code:</label>
-                    <input type="text" name="postal_code" value={formData.postal_code} onChange={handleChange} placeholder="Enter Postal Code" className="block w-full border-gray-300 rounded-md focus:ring-indigo-500 focus:border-indigo-500 mb-4 p-2" />
+                        <label className="block mb-2">Postal Code:</label>
+                        <input autoComplete="off" type="text" name="postal_code" value={formData.postal_code} onChange={handleChange} placeholder="Enter Postal Code" className="block w-full border-gray-300 rounded-md focus:ring-indigo-500 focus:border-indigo-500 mb-4 p-2" />
 
-                    <label className="block mb-2">Location:</label>
-                    <input type="text" name="location" value={formData.location} onChange={handleChange} placeholder="Enter Location" className="block w-full border-gray-300 rounded-md focus:ring-indigo-500 focus:border-indigo-500 mb-4 p-2" />
+                        <label className="block mb-2">Location:</label>
+                        <input type="text" name="location" value={formData.location} onChange={handleChange} placeholder="Enter Location" className="block w-full border-gray-300 rounded-md focus:ring-indigo-500 focus:border-indigo-500 mb-4 p-2" />
 
-                    <label className="block mb-2">Category ID:</label>
-                    <input type="text" name="category_id" value={formData.category_id} onChange={handleChange} className="block w-full border-gray-300 rounded-md focus:ring-indigo-500 focus:border-indigo-500 mb-4 p-2" />
+                        <label className="block mb-2">Category ID:</label>
+                        <input type="text" name="category_id" value={formData.category_id} onChange={handleChange} className="block w-full border-gray-300 rounded-md focus:ring-indigo-500 focus:border-indigo-500 mb-4 p-2" />
 
-                    <label className="block mb-2">Description:</label>
-                    <textarea name="description" value={formData.description} onChange={handleChange} className="block w-full border-gray-300 rounded-md focus:ring-indigo-500 focus:border-indigo-500 mb-4 p-2"></textarea>
-                    
-                    <button type="submit" className="bg-indigo-500 text-white py-2 px-4 rounded-md hover:bg-indigo-600">Submit</button>
-                </div>
-                <div className='rightFormSection'> 
-                    <div className="imageUploadSection">
-                        <label htmlFor="dropzone-file" className="flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600">
-                            <div className="flex flex-col items-center justify-center pt-5 pb-6">
-                                <svg className="w-8 h-8 mb-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 16">
-                                    <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"/>
-                                </svg>
-                                <p className="mb-2 text-sm text-gray-500 dark:text-gray-400"><span className="font-semibold">Click to upload</span> or drag and drop</p>
-                                <p className="text-xs text-gray-500 dark:text-gray-400">SVG, PNG, JPG or GIF (MAX. 800x400px)</p>
-                            </div>
-                            <input
-                                type="file"
-                                id="dropzone-file"
-                                name="avatar"
-                                accept=".jpg, .png, .jpeg"
-                                multiple
-                                onChange={(e) => checkFileForImage(e.target.files)}
-                                className=''
-                            />
-                        </label>
-                    </div> 
-                    <div>
-                        <h1>Images Selected:</h1>
-                        {selectedImages.map((elem, index) => (
-                            <img src={elem} key={index}></img>
-                        ))}
+                        <label className="block mb-2">Description:</label>
+                        <textarea rows="3" name="description" value={formData.description} onChange={handleChange} className="block w-full border-gray-300 rounded-md focus:ring-indigo-500 focus:border-indigo-500 mb-4 p-2"></textarea>
+                        
                     </div>
+                    <div className='rightFormSection'> 
+                        <div className="imageUploadSection">
+                            <label htmlFor="dropzone-file" className="flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600">
+                                <div className="flex flex-col items-center justify-center pt-5 pb-6">
+                                    <svg className="w-8 h-8 mb-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 16">
+                                        <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"/>
+                                    </svg>
+                                    <p className="mb-2 text-sm text-gray-500 dark:text-gray-400"><span className="font-semibold">Click to upload</span> or drag and drop</p>
+                                    <p className="text-xs text-gray-500 dark:text-gray-400">SVG, PNG, JPG or GIF (MAX. 800x400px)</p>
+                                </div>
+                                <input
+                                    type="file"
+                                    id="dropzone-file"
+                                    name="avatar"
+                                    accept=".jpg, .png, .jpeg"
+                                    multiple
+                                    onChange={(e) => checkFileForImage(e.target.files)}
+                                    className='hidden'
+                                />
+                            </label>
+                        </div> 
+                        <div>
+                            <h1>Images Selected:</h1>
+                            {selectedImages.map((elem, index) => (
+                                <img src={elem} key={index}></img>
+                            ))}
+                        </div>
+                    </div>
+                </div>
+                <div className='bottomFormSection'>
+                    <button type="submit" className="bg-indigo-500 text-white py-2 px-4 rounded-md hover:bg-indigo-600">Submit</button>
+                    <button className="bg-red-500 text-white py-2 px-4 rounded-md hover:bg-red-600">Cancel</button>
                 </div>
             </form>
 
