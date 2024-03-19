ded9d1af44178ef51c11bfef32002173e21ae2ba
Emily M <105607751+Emil-mm@users.noreply.github.com>
2024-03-19 00:51:05 -0400

    Responsive fonts

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -48,29 +48,29 @@ export default function LoginPage() {
 
     return (
         <section className='flex justify-center items-center h-[100vh] bg-neutral-100'>
-            <form onSubmit={handleSubmit} className='flex bg-amber-50 shadow-lg rounded-xl border-2 border-amber-200 w-[70%] sm:w-[55%] md:w-[45%] lg:w-[35%] px-8 py-4 flex-wrap space-y-4 justify-center'>
+            <form onSubmit={handleSubmit} className='flex bg-amber-50 shadow-lg rounded-xl border-2 border-amber-200 w-[70%] sm:w-[55%] md:w-[45%] lg:w-[35%] px-8 py-4 m-4 flex-wrap space-y-4 justify-center'>
                 <img src="../assets/logo-light.png" alt="Logo" className="h-full w-[30%] m-auto"></img>
                 <section className='w-full space-y-2'>
                     <div className='flex-col w-[90%]'>
                         <label htmlFor={"email"} className={"block mb-2 text-sm font-medium text-gray-900"}>Email <span className='text-neutral-400 text-xs'>(Must use TMU Email)</span></label>
                         <input  type="email" name="email"
-                            className={"bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-1.5"} 
+                            className={"bg-gray-50 border border-gray-300 text-gray-900 text-xs sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-1.5"} 
                             placeholder="name@torontomu.ca" required>
                         </input>
-                        {errors.email && <div className={"mb-1 text-sm font-medium text-red-600 text-wrap"}>{errors.email}</div>}
+                        {errors.email && <div className={"mb-1 text-xs sm:text-sm font-medium text-red-600 text-wrap"}>{errors.email}</div>}
                     </div>
                     <div className='flex-col w-[90%]'>
                         <label htmlFor={"password"} className={"block mb-2 text-sm font-medium text-gray-900"}>Password</label>
                         <input  type="password" name="password"
-                            className={"bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-1.5"} 
+                            className={"bg-gray-50 border border-gray-300 text-gray-900 text-xs sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-1.5"} 
                             placeholder="Enter password" required minLength={6}>
                         </input>
-                        {errors.password && <div className={"mb-1 text-sm font-medium text-red-600"}>{errors.password}</div>}
+                        {errors.password && <div className={"mb-1 text-xs sm:text-sm font-medium text-red-600 text-wrap"}>{errors.password}</div>}
                     </div>
                 </section>
-                <section className='flex flex-col space-y-1.5'>
-                    <button className={" bg-sky-500 rounded w-32 shadow hover:bg-sky-600 text-white py-2"} type='submit'>Log in</button>
-                    <Link to={'/register'} className='hover:underline'>{'Not registered?'}</Link>
+                <section className='flex flex-col text-center space-y-1.5'>
+                    <button className={" bg-sky-500 rounded text-sm sm:text-base w-32 shadow hover:bg-sky-600 text-white py-2"} type='submit'>Log in</button>
+                    <Link to={'/register'} className='text-sm sm:text-base hover:underline'>{'Not registered?'}</Link>
                 </section>
             </form>
         </section>
--- client/src/pages/RegisterPage.jsx
+++ client/src/pages/RegisterPage.jsx
@@ -60,22 +60,22 @@ export default function RegisterPage() {
     }, [user])
 
     return (
-        <section className='flex justify-center items-center h-[100vh] bg-neutral-100'>
-            <form onSubmit={handleSubmit} className='flex bg-amber-50 shadow-lg rounded-xl border-2 border-amber-200 w-[70%] sm:w-[55%] md:w-[45%] lg:w-[35%] px-8 py-4 flex-wrap space-y-4 justify-center'>
+        <section className='flex justify-center items-center h-auto sm:h-[100vh] bg-neutral-100'>
+            <form onSubmit={handleSubmit} className='flex bg-amber-50 shadow-lg rounded-xl border-2 border-amber-200 w-[70%] sm:w-[55%] md:w-[45%] lg:w-[35%] px-8 py-4 m-4 flex-wrap space-y-4 justify-center'>
                 <img src="../assets/logo-light.png" alt="Logo" className="h-full w-[30%] m-auto"></img>
                 <section className='w-full space-y-2'>
                     <div className='flex flex-col sm:flex-row space-y-2 sm:space-y-0 sm:space-x-[6%] w-[90%]'>
                         <div className='flex-col w-full sm:w-[47%]'>
                             <label htmlFor={"first-name"} className={"block mb-2 text-sm font-medium text-gray-900"}>First Name <span className='text-neutral-400 text-xs'>(Letters Only)</span></label>
                             <input  type="text" name="first-name"
-                                className={" bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-1.5"} 
+                                className={"bg-gray-50 border border-gray-300 text-gray-900 text-xs sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-1.5"} 
                                 placeholder="Enter First Name" required pattern='[A-Za-z]+' minLength={2}>
                             </input>
                         </div>
                         <div className='flex-col w-full sm:w-[47%]'>
                             <label htmlFor={"last-name"} className={"block mb-2 text-sm font-medium text-gray-900"}>Last Name <span className='text-neutral-400 text-xs'>(Letters Only)</span></label>
                             <input  type="text" name="last-name"
-                                className={"bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-1.5"} 
+                                className={"bg-gray-50 border border-gray-300 text-gray-900 text-xs sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-1.5"} 
                                 placeholder="Enter Last Name" required pattern='[A-Za-z]+' minLength={2}>
                             </input>
                         </div>
@@ -83,48 +83,48 @@ export default function RegisterPage() {
                     <div className='flex-col w-[90%]'>
                         <label htmlFor={"student-num"} className={"block mb-2 text-sm font-medium text-gray-900"}>Student Number <span className='text-neutral-400 text-xs'>(9 digits, no spaces)</span></label>
                         <input  type="text" name="student-num"
-                            className={"bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-1.5"} 
+                            className={"bg-gray-50 border border-gray-300 text-gray-900 text-xs sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-1.5"} 
                             placeholder="Enter Student Number" required pattern='[0-9]{9}' minLength={9} maxLength={9}>
                         </input>
                     </div>
                     <div className='flex-col w-[90%]'>
                         <label htmlFor={"username"} className={"block mb-2 text-sm font-medium text-gray-900"}>Username <span className='text-neutral-400 text-xs'>(No spaces)</span></label>
                         <input  type="text" name="username"
-                            className={"bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-1.5"} 
+                            className={"bg-gray-50 border border-gray-300 text-gray-900 text-xs sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-1.5"} 
                             placeholder="Enter username" required pattern='[A-Za-z0-9]{4,16}' minLength={4} maxLength={16}>
                         </input>
-                        {errors.username && <div className={"mb-1 text-sm font-medium text-red-600"}>{errors.username}</div>}
+                        {errors.username && <div className={"mb-1 text-xs sm:text-sm font-medium text-red-600"}>{errors.username}</div>}
                     </div>
                     <div className='flex-col w-[90%]'>
                         <label htmlFor={"email"} className={"block mb-2 text-sm font-medium text-gray-900"}>Email <span className='text-neutral-400 text-xs'>(Must use TMU Email)</span></label>
                         <input  type="email" name="email"
-                            className={"bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-1.5"} 
+                            className={"bg-gray-50 border border-gray-300 text-gray-900 text-xs sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-1.5"} 
                             placeholder="name@torontomu.ca" required>
                         </input>
-                        {errors.email && <div className={"mb-1 text-sm font-medium text-red-600"}>{errors.email}</div>} 
+                        {errors.email && <div className={"mb-1 text-xs sm:text-sm font-medium text-red-600"}>{errors.email}</div>} 
                     </div>
                     <div className='flex flex-col sm:flex-row space-y-2 sm:space-y-0 sm:space-x-[6%] w-[90%]'>
                         <div className='flex-col w-full sm:w-[47%]'>
                             <label htmlFor={"password"} className={"block mb-2 text-sm font-medium text-gray-900"}>Password</label>
                             <input  type="password" name="password"
-                                className={"bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-1.5"} 
+                                className={"bg-gray-50 border border-gray-300 text-gray-900 text-xs sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-1.5"} 
                                 placeholder="Enter password" required minLength={6}>
                             </input>
-                            {errors.password && <div className={"mb-1 text-sm font-medium text-red-600"}>{errors.password}</div>}
+                            {errors.password && <div className={"mb-1 text-xs sm:text-sm font-medium text-red-600"}>{errors.password}</div>}
                         </div>
                         <div className='flex-col w-full sm:w-[47%]'>
                             <label htmlFor={"confirm-password"} className={"block mb-2 text-sm font-medium text-gray-900"}>Confirm Password</label>
                             <input  type="password" name="confirm-password"
-                                className={"bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-1.5"} 
+                                className={"bg-gray-50 border border-gray-300 text-gray-900 text-xs sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-1.5"} 
                                 placeholder="Re-enter password" required minLength={6}>
                             </input>
-                            {errors.confirm && <div className={"mb-1 text-sm font-medium text-red-600"}>{errors.confirm}</div>}
+                            {errors.confirm && <div className={"mb-1 text-xs sm:text-sm font-medium text-red-600"}>{errors.confirm}</div>}
                         </div>
                     </div>
                 </section>
-                <section className='flex flex-col space-y-1.5'>
-                    <button className={" bg-sky-500 rounded w-32 shadow hover:bg-sky-600 text-white py-2"} type='submit'>Register</button>
-                    <Link to={'/login'} className='hover:underline'>{'Already registered?'}</Link>
+                <section className='flex flex-col text-center space-y-1.5'>
+                    <button className={" bg-sky-500 rounded text-sm sm:text-base w-32 shadow hover:bg-sky-600 text-white py-2"} type='submit'>Register</button>
+                    <Link to={'/login'} className='text-sm sm:text-base hover:underline'>{'Already registered?'}</Link>
                 </section>
             </form>
         </section>
