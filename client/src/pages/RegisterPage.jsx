4d14bff92dcf7193387f707a754af33bf724be49
Emily M <105607751+Emil-mm@users.noreply.github.com>
2024-03-18 21:55:17 -0400

    Added Name to Register Form and changed padding.

--- client/src/pages/RegisterPage.jsx
+++ client/src/pages/RegisterPage.jsx
@@ -58,29 +58,43 @@ export default function RegisterPage() {
 
     return (
         <section className='flex justify-center items-center h-[100vh] bg-neutral-100'>
-            <form onSubmit={handleSubmit} className='flex bg-amber-50 shadow-lg rounded-xl border-2 border-amber-200 w-[70%] sm:w-[55%] md:w-[45%] lg:w-[35%] p-8 flex-wrap space-y-4 justify-center'>
+            <form onSubmit={handleSubmit} className='flex bg-amber-50 shadow-lg rounded-xl border-2 border-amber-200 w-[70%] sm:w-[55%] md:w-[45%] lg:w-[35%] px-8 py-3 flex-wrap space-y-4 justify-center'>
                 <h2 className='w-full text-xl text-center'>Logo</h2>
-                <section className='w-full space-y-2'>
+                <section className='w-full space-y-1.5'>
                     <div className='flex-col w-[80%]'>
-                        <label htmlFor={"username"} className={"block mb-2 text-sm font-medium text-gray-900"}>Username <span className='text-neutral-400'>(No spaces)</span></label>
-                        <input  type="text" name="username"
-                            className={"bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"} 
-                            placeholder="Enter username" required pattern='[A-Za-z0-9]{4,16}' minLength={4} maxLength={16}>
-                        </input>
+                        <label htmlFor={"full-name"} className={"block mb-2 text-sm font-medium text-gray-900"}>First Name <span className='text-neutral-400'>(Letters Only)</span></label>
+                        <div className='flex flex-row space-x-[6%]'>
+                            <input  type="text" name="first-name"
+                                className={" bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-[47%] p-2"} 
+                                placeholder="Enter First Name" required pattern='[A-Za-z]+' minLength={2}>
+                            </input>
+                            <input  type="text" name="last-name"
+                                className={"bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-[47%] p-2"} 
+                                placeholder="Enter Last Name" required pattern='[A-Za-z]+' minLength={2}>
+                            </input>
+                        </div>
                         {errors.username && <div className={"mb-2 text-sm font-medium text-red-600"}>{errors.username}</div>}
                     </div>
                     <div className='flex-col w-[80%]'>
                         <label htmlFor={"student-num"} className={"block mb-2 text-sm font-medium text-gray-900"}>Student Number <span className='text-neutral-400'>(9 digits)</span></label>
                         <input  type="text" name="student-num"
-                            className={"bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"} 
+                            className={"bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2"} 
                             placeholder="Enter Student Number" required pattern='[0-9]{9}' minLength={9} maxLength={9}>
+                        </input>
+                        {errors.username && <div className={"mb-2 text-sm font-medium text-red-600"}>{errors.username}</div>}
+                    </div>
+                    <div className='flex-col w-[80%]'>
+                        <label htmlFor={"username"} className={"block mb-2 text-sm font-medium text-gray-900"}>Username <span className='text-neutral-400'>(No spaces)</span></label>
+                        <input  type="text" name="username"
+                            className={"bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2"} 
+                            placeholder="Enter username" required pattern='[A-Za-z0-9]{4,16}' minLength={4} maxLength={16}>
                         </input>
                         {errors.username && <div className={"mb-2 text-sm font-medium text-red-600"}>{errors.username}</div>}
                     </div>
                     <div className='flex-col w-[80%]'>
                         <label htmlFor={"email"} className={"block mb-2 text-sm font-medium text-gray-900"}>Email <span className='text-neutral-400'>(Must use TMU Email)</span></label>
                         <input  type="email" name="email"
-                            className={"bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"} 
+                            className={"bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2"} 
                             placeholder="name@torontomu.ca" required>
                         </input>
                         {errors.email && <div className={"mb-2 text-sm font-medium text-red-600"}>{errors.email}</div>} 
@@ -88,7 +102,7 @@ export default function RegisterPage() {
                     <div className='flex-col w-[80%]'>
                         <label htmlFor={"password"} className={"block mb-2 text-sm font-medium text-gray-900"}>Password</label>
                         <input  type="password" name="password"
-                            className={"bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"} 
+                            className={"bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2"} 
                             placeholder="Enter password" required minLength={6}>
                         </input>
                         {errors.password && <div className={"mb-2 text-sm font-medium text-red-600"}>{errors.password}</div>}
@@ -96,13 +110,13 @@ export default function RegisterPage() {
                     <div className='flex-col w-[80%]'>
                         <label htmlFor={"confirm-password"} className={"block mb-2 text-sm font-medium text-gray-900"}>Confirm Password</label>
                         <input  type="password" name="confirm-password"
-                            className={"bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"} 
+                            className={"bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2"} 
                             placeholder="Re-enter password" required minLength={6}>
                         </input>
                         {errors.confirm && <div className={"mb-2 text-sm font-medium text-red-600"}>{errors.confirm}</div>}
                     </div>
                 </section>
-                <section className='flex flex-col space-y-3'>
+                <section className='flex flex-col space-y-2'>
                     <button className={" bg-sky-500 rounded w-32 shadow hover:bg-sky-600 text-white py-2"} type='submit'>Register</button>
                     <Link to={'/login'} className='hover:underline'>{'Already registered?'}</Link>
                 </section>
