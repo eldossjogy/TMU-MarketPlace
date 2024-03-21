c0eb229e570e5f2c77d90eb293f68b3ed4ee6dc0
eldossjogy <eldossjogy@gmail.com>
2024-03-20 22:29:11 -0400

    Fixed no-image issue

---
+++ client/src/components/CardImages.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import ImageCarousel from "./ImageCarousel";
+import noImage from '../assets/noImage.png'
+
+export default function CardImages({ image, hovered, setHovered }) {
+  if (image.length === 0) {
+    return (
+      <img
+        className="rounded-md w-full h-auto object-cover  aspect-square"
+        src={noImage}
+        alt="img"
+      ></img>
+    );
+  }
+
+  if (image.length > 1) {
+    return (
+      <ImageCarousel images={image} hovered={hovered} setHovered={setHovered} />
+    )
+  } else {
+    return (
+      <img
+        className="rounded-md w-full h-auto object-cover  aspect-square"
+        src={image[0].file_path}
+        alt="img"
+      ></img>
+    );
+  }
+}
