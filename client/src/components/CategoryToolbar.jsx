65390431857af0ca9781324361d2a5da3a5431c2
aminnausin <aminnausin@outlook.com>
2024-03-28 16:25:04 -0400

    make toolbars fully rounded

--- client/src/components/CategoryToolbar.jsx
+++ client/src/components/CategoryToolbar.jsx
@@ -18,13 +18,13 @@ export default function CategoryToolbar({getFunc}) {
                 <RadioGroup value={activeIndex} className={"flex flex-wrap items-center"}>
                     <RadioGroup.Option key={-1} value={null} >
                         {({ checked }) => (
-                            <ToolbarButton key={-1} stateID={null} value={"All"} checked={checked} onClick={handleClick} baseStyle={'px-3 py-1 text-sm md:px-4 md:text-base lg:text-lg 2xl:text-xl 2xl:px-6 2xl:py-2 my-1 me-2'}/>
+                            <ToolbarButton key={-1} stateID={null} value={"All"} checked={checked} onClick={handleClick} baseStyle={'px-3 py-1 text-sm md:px-4 md:text-base lg:text-lg 2xl:text-xl 2xl:px-6 2xl:py-2 my-1 me-2 rounded-full'}/>
                         )}
                     </RadioGroup.Option>
                     {categories && categories.map((sortKey) => (
                         <RadioGroup.Option key={sortKey.id} value={sortKey.id} >
                             {({ checked }) => (
-                                <ToolbarButton key={sortKey.id} stateID={sortKey.id} value={sortKey.name} checked={checked} onClick={handleClick} baseStyle={'px-3 py-1 text-sm md:px-4 md:text-base lg:text-lg 2xl:text-xl 2xl:px-6 2xl:py-2 my-1 me-2'}/>
+                                <ToolbarButton key={sortKey.id} stateID={sortKey.id} value={sortKey.name} checked={checked} onClick={handleClick} baseStyle={'px-3 py-1 text-sm md:px-4 md:text-base lg:text-lg 2xl:text-xl 2xl:px-6 2xl:py-2 my-1 me-2 rounded-full'}/>
                             )}
                         </RadioGroup.Option>
                     ))}
--- client/src/components/ToolbarButton.jsx
+++ client/src/components/ToolbarButton.jsx
@@ -1,12 +1,12 @@
 import { BarsArrowDownIcon, BarsArrowUpIcon } from '@heroicons/react/24/solid'
 import React from 'react'
 
-export default function ToolbarButton({stateID, value, checked, onClick, stateValue = 0, baseStyle = 'px-4 py-1 my-1 me-2 text-sm', useIcon = true}) {
+export default function ToolbarButton({stateID, value, checked, onClick, stateValue = 0, baseStyle = 'px-4 py-1 my-1 me-2 text-sm rounded-2xl', useIcon = true}) {
 
 
     return (
         <>
-            <button className={`flex items-center justify-center ring-2 ring-inset rounded-2xl space-x-1 ${baseStyle} ${checked ? 'ring-yellow-500 hover:bg-yellow-400 bg-amber-400' : 'ring-sky-600 hover:text-white hover:bg-sky-500'}`} onClick={() => {
+            <button className={`flex items-center justify-center ring-2 ring-inset space-x-1 ${baseStyle} ${checked ? 'ring-yellow-500 hover:bg-yellow-400 bg-amber-400' : 'ring-sky-600 hover:text-white hover:bg-sky-500'}`} onClick={() => {
                     onClick(stateID)
                 }}>
                 <span>{value}</span>
