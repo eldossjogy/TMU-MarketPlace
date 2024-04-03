007f0484adec247bd053e86c5f3294eab531aa01
aminnausin <aminnausin@outlook.com>
2024-04-03 16:28:13 -0400

    temp chat

--- client/src/components/ChatMessage.jsx
+++ client/src/components/ChatMessage.jsx
@@ -1,34 +1,48 @@
 import React from 'react'
 
 export default function ChatMessage(
-    {sender = true, message = '', initial = 'A', timestamp = ''}) {
-  return (
-    <>
-        {!sender && (<div className='flex p-1'>
-                <div className='flex-col space-y-2' >
-                    <div className='flex gap-2'>
-                        <div className='flex items-center justify-center rounded-full h-8 w-8 aspect-square bg-rose-500'>{initial}</div>
-                        <div className='flex items-center justify-start text-gray-400 line-clamp-1 truncate'>{timestamp}</div>
+    { sender = true, message = '', username = 'A', timestamp = '' }) {
+
+
+    const rawDate = new Date(timestamp ?? '01/16/2024');
+    const rawAge = Date.now() - rawDate.getTime();
+
+    const weeks = Math.round(rawAge / (1000 * 3600 * 24 * 7));
+    const days = Math.round(rawAge / (1000 * 3600 * 24));
+    const hours = Math.round(rawAge / (1000 * 3600));
+    const minutes = Math.round(rawAge / (1000 * 60));
+    const seconds = Math.round(rawAge / (1000));
+
+    const age = weeks > 0 ? `${weeks} week${weeks > 1 ? 's' : ''} ago` : days > 0 ? `${days} day${days > 1 ? 's' : ''} ago` : hours > 0 ? `${hours} hour${hours > 1 ? 's' : ''} ago` : minutes > 0 ? `${minutes}m ago` : `${seconds}s ago`
+
+    return (
+        <>
+            {!sender && (
+                <div className='flex p-1 max-w-full'>
+                    <div className='flex-col space-y-2' >
+                        <div className='flex gap-2'>
+                            <div className='flex items-center justify-center rounded-full h-8 w-8 aspect-square bg-rose-500'>{username[0]}</div>
+                            <div className='flex items-center justify-start text-gray-400 line-clamp-1 truncate'>{age}</div>
+                        </div>
+                        <div className='bg-neutral-200 rounded-xl flex items-center justify-start p-2 w-auto'>{message}</div>
                     </div>
-                    <div className='bg-neutral-200 rounded-xl flex items-center justify-start p-2 w-auto'>{message}</div>
-                </div>
-                <div className='w-[30%] shrink-0'>
-                </div>
-            </div>
-        )}
-        {sender && (
-            <div className='flex p-1 flex-row-reverse'>
-                <div className='flex-col space-y-2 w-ful' >
-                    <div className='flex gap-2 flex-row-reverse'>
-                        <div className='flex items-center justify-center rounded-full h-8 w-8 aspect-square bg-violet-400'>{initial}</div>
-                        <div className='flex items-center justify-start text-gray-400 line-clamp-1 truncate'>{timestamp}</div>
+                    <div className='w-[30%] shrink-0'>
                     </div>
-                    <div className='bg-sky-200 rounded-xl flex items-center justify-start p-2 w-auto'>{message}</div>
                 </div>
-                <div className='w-[30%] shrink-0'>
+            )}
+            {sender && (
+                <div className='flex p-1 flex-row-reverse max-w-full'>
+                    <div className='flex-col space-y-2 w-full' >
+                        <div className='flex gap-2 flex-row-reverse'>
+                            <div className='flex items-center justify-center rounded-full h-8 w-8 aspect-square bg-violet-400'>{username[0]}</div>
+                            <div className='flex items-center justify-start text-gray-400 line-clamp-1 truncate'>{age}</div>
+                        </div>
+                        <div className='bg-sky-200 rounded-xl flex items-center justify-start p-2 w-auto'>{message}</div>
+                    </div>
+                    <div className='w-[30%] shrink-0'>
+                    </div>
                 </div>
-            </div>
-        )}
-    </>
-  )
+            )}
+        </>
+    )
 }
--- client/src/components/Chatbox.jsx
+++ client/src/components/Chatbox.jsx
@@ -1,11 +1,22 @@
 import React, { useContext, useEffect, useState } from 'react';
 import ChatMessage from './ChatMessage';
 import ChatContext from "../authAndContext/chatProvider";
+import AuthContext from '../authAndContext/contextApi';
 
 export default function Chatbox() {
-    const {messages} = useContext(ChatContext)
-    //const [messages, setMessages] = useState([]);
+    const { sentMsg, getChat, messages, removeNotification, currentChat, gotMail, exitChat } = useContext(ChatContext)
+    const { user } = useContext(AuthContext)
+    const [reply,setReply] = useState({id: null, ad_id: null})
     const [nextMessage, setNextMessage] = useState('');
+
+    useEffect(() => { getChat() }, [user])
+    /* call on unmount */
+    useEffect(() => {
+        return () => {
+        exitChat()
+        };
+    }, []); // Empty dependency array means this effect runs only once (on mount) and cleans up on unmount
+
     const sendMessage = (e) => {
         e?.preventDefault();
 
@@ -15,6 +26,7 @@ export default function Chatbox() {
         //     setNextMessage('');
         // }
     }
+    
     return (
         <div className='flex flex-col group w-full lg:w-[40%] rounded-xl  border-2 border-gray shadow-md hover:shadow-lg h-[60vh] divide-y-2 overflow-hidden'>
             <section className='w-full p-3 h-24 bg-[#fafafb] rounded-xl'>
@@ -22,13 +34,25 @@ export default function Chatbox() {
                 <h3 className='text-xl'>Adam</h3>
             </section>
             <section className='flex flex-col w-full bg-white p-3 overflow-y-auto overflow-x-hidden h-[80%]'>
-                {messages && messages.map((msg) => (
-                    <ChatMessage key={msg.id} message={msg.message} sender={msg.sender} initial={msg.initial} timestamp={msg.timestamp}/>
-                ))}
+                {messages && messages.map((msg) => {
+                    if (currentChat === msg.chat_id) {
+                        // if (reply.id != ele.sender_id && ele.sender_id != user.id) {
+                        //     setReply((prev) => ({ ad_id: ele?.chats?.ad_id, id: ele.sender_id }))
+                        // }
+
+                        return (
+                            // <div key={msg.id}>
+                                /* {msg.chat_id} -- {msg?.chats?.ad_id} -- {msg.sender?.name} - ={msg.sender_id}= - {msg.message} */
+                            <ChatMessage key={msg.id} message={msg.message} sender={msg.sender_id === user.id} initial={msg.sender?.name[0]} timestamp={'msg.created_at'} />
+
+                            // </div>
+                        );
+                    }
+                })}
             </section>
             <form className='w-full bg-white p-3 h-auto shrink-0 flex justify-center items-center flex-row-reverse z-50 gap-2 rounded-xl' onSubmit={sendMessage}>
                 <button className="w-full rounded-xl shadow-md h-full bg-[#eebe45] hover:bg-[#f9a200] text-neutral-950 flex justify-center items-center py-1 max-w-20">Reply</button>
-                <input className='w-full bg-neutral-100 rounded-2xl border-gray-200' placeholder='Send a message' name='message' aria-label='message' value={nextMessage} onChange={(e) => {setNextMessage(e.target.value);}}></input>
+                <input className='w-full bg-neutral-100 rounded-2xl border-gray-200' placeholder='Send a message' name='message' aria-label='message' value={nextMessage} onChange={(e) => { setNextMessage(e.target.value); }}></input>
             </form>
         </div>
     )
