7ec1979a953938b361fea8319e3accd40b94f79c
Emily M <105607751+Emil-mm@users.noreply.github.com>
2024-04-03 23:58:20 -0400

    Mobile screen full screen

--- client/src/pages/RegisterPage.jsx
+++ client/src/pages/RegisterPage.jsx
@@ -93,8 +93,8 @@ export default function RegisterPage() {
     }
 
     return (
-        <div className="bg-gray-50 h-auto min-h-[100vh] flex items-center justify-center px-6 py-8 mx-auto lg:py-3">
-            <div className="w-full bg-white rounded-lg shadow-lg md:mt-0 sm:max-w-md xl:p-0">
+        <div className="bg-gray-50 h-auto min-h-[100vh] flex items-center justify-center p-0 sm:px-6 sm:py-8 mx-auto lg:py-3">
+            <div className="w-full h-full sm:h-auto bg-white rounded-lg shadow-lg md:mt-0 sm:max-w-md xl:p-0">
                 <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
                     <section className="h-16 w-auto md:w-64 xl:w-80 m-auto">
                         <img src={transparentLogo} alt="Logo" className="h-full w-auto m-auto"></img>
